158b5c0c01e874b7f0c6308874ebb1f00c18bea5
Edward Dupre <edwd42@gmail.com>
2018-08-03 12:25:10 -0400

    add test when2NumbersAreUsedThenNoExceptionIsThrown()

--- test/net/ed/StringCalculatorTest.java
+++ test/net/ed/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package net.ed;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,7 +10,14 @@ public class StringCalculatorTest {
 
     @Test(expected = RuntimeException.class)
     public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
-        StringCalculator.add("1,2,xx");
+        StringCalculator.add("1,2,3");
+        Assert.assertTrue(true);
+    }
+    
+    @Test // (expected = RuntimeException.class)
+    public final void when2NumbersAreUsedThenNoExceptionIsThrown() {
+        StringCalculator.add("1,2");
+        Assert.assertTrue(true);
     }
 
 }
