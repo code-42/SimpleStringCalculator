e9aa80e7c2140978b8b72e81d7607f28f8062afd
Edward Dupre <edwd42@gmail.com>
2018-08-03 12:29:36 -0400

    add test whenNonNumberIsUsedThenExceptionIsThrown()

--- src/net/ed/StringCalculator.java
+++ src/net/ed/StringCalculator.java
@@ -6,16 +6,13 @@ public class StringCalculator {
 
 	public static void add(String numbers) {
 		
-		System.out.println("9. inside add()");
-		// TODO Auto-generated method stub
 		String[] numbersArray = numbers.split(",");
-		System.out.println(numbersArray.length);
 		if (numbersArray.length > 2) {
 			throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
 		} else {
 			for (String number : numbersArray) {
 				Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
-				System.out.println(Integer.parseInt(number));
+				System.out.println("numbers == " + Integer.parseInt(number));
 			}
 		}
 		
--- test/net/ed/StringCalculatorTest.java
+++ test/net/ed/StringCalculatorTest.java
@@ -19,5 +19,11 @@ public class StringCalculatorTest {
         StringCalculator.add("1,2");
         Assert.assertTrue(true);
     }
+    
+    @Test(expected = RuntimeException.class)
+    public final void whenNonNumberIsUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,X");
+        Assert.assertTrue(true);
+    }
 
 }
